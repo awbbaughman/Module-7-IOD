17ae8ebd6fd5b0f2c75a663c4f23e83e2b43a198
awbbaughman <144296345+awbbaughman@users.noreply.github.com>
2024-04-27 12:26:43 -0400

    Add files via upload

---
+++ Lab 4/Pages/Login.jsx
@@ -0,0 +1,10 @@
+import { React } from 'react'
+
+export default function LoginPage() {
+    return (
+        <div className="Login">
+            <h1>Login Page here</h1>
+            
+        </div>
+    )
+}
